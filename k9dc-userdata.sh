d4390474aafa7863fcf2e3503ceaa4c67af630b1
vdonkor <vincent.donkor@gmail.com>
2019-06-30 20:39:18 -0400

    updated k9dc

--- k9dc-userdata.sh
+++ k9dc-userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum -y install wget git python-setuptools s3cmd
 easy_install pip
-pip install ansible 
+pip install ansible boto boto3 botocore
 git clone https://github.com/vdonkor/ctn.git
 cd ctn
 ansible-playbook k9dc.yml 
