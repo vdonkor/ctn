e890f79f10a1ca8bb3a9aa1b39471af59aec700d
vdonkor <vincent.donkor@gmail.com>
2019-07-05 18:34:40 -0400

    fixed heredock issues

--- jobs/icdc/pipeline.groovy
+++ jobs/icdc/pipeline.groovy
@@ -1,9 +1,5 @@
 pipelineJob('icdc/k9dc') {
-  parameters {
-  gitParameter branch: '', branchFilter: '.*', defaultValue: 'master', description: 'Choose the tag or branch to deploy', name: 'Tag', quickFilterEnabled: false, selectedValue: 'DEFAULT', sortMode: 'ASCENDING_SMART', tagFilter: '*', type: 'PT_TAG'
-  }
-
-
+  
   def repo = 'https://github.com/vdonkor/ctn.git' 
   description("ICDC pipeline Job") 
 
