492fab3fb1a8b1cfe2779f74da16babc6e14706c
vdonkor <vincent.donkor@gmail.com>
2019-07-11 01:34:56 -0400

    added newrelic

--- boostrap.sh
+++ boostrap.sh
@@ -5,5 +5,4 @@ yum -y install wget git python-setuptools
 easy_install pip
 pip install ansible
 git clone https://github.com/vdonkor/ctn.git
-echo "mypassword" > ./ctn/mypassword
-ansible-playbook -i ./ctn/hosts --vault-password-file ./ctn/mypassword ./ctn/playbook.yml 
+ansible-playbook -i ./ctn/hosts --vault-password-file ./ctn/mypassword ./ctn/newrelic.yml -e newrelic_license_key
